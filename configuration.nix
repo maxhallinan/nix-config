ef1df8cfe1a415696b0e9c115fafab38bc3b26cb
Max Hallinan <maxhallinan@gmail.com>
2023-04-07 10:58:13 -0400

    Add m1 configuration

---
+++ configuration.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, home-manager, ... }:
+
+{
+  imports = [
+    home-manager.darwinModule
+  ];
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.vim
+      pkgs.alacritty
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  programs.zsh.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  users.users = {
+    maxhallinan = {
+      name = "maxhallinan";
+      home = "/Users/maxhallinan";
+      shell = pkgs.zsh;
+    };
+  };
+
+  home-manager = {
+    useUserPackages = true;
+    verbose = true;
+    users.maxhallinan = import ./home-manager/home.nix;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "maxhallinan-nix-config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    darwin,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    darwinConfigurations = {
+      "maxhallinan-m1" = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [./configuration.nix];
+        specialArgs = inputs;
+      };
+    };
+  };
+}
