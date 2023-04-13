28bfc04d905349dc44721f717d1a1471812bf29f
Max Hallinan <maxhallinan@gmail.com>
2023-04-13 12:38:10 -0400

    Add iterm2 and postman to homebrew casks.

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -2,13 +2,17 @@
   homebrew = {
     enable = true;
     onActivation.upgrade = true;
+    casks = [
+      "iterm2"
+      "postman"
+    ];
     brews = [
+      "postgis"
       {
         name = "postgresql@14";
         start_service = true;
         restart_service = true;
       }
-      "postgis"
     ];
   };
 }
