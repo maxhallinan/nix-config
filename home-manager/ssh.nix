11369eac0b52741b7e2e6afacfc2758e226c9305
Max Hallinan <maxhallinan@gmail.com>
2023-04-28 11:37:10 -0400

    Add Mercury hosts to ssh config

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -6,6 +6,43 @@
     extraConfig = ''
       Host *
         IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+      Host *.internal.mercury.co *.internal.mercury.com mercury-*
+          User maxhallinan
+          IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+      Host mercury-staging
+          HostName 1.mercury-web-backend.staging.internal.mercury.com
+
+      Host mercury-staging-prod
+          HostName 1.mercury-web-backend.staging-prod.internal.mercury.com
+
+      Host mercury-staging-earth
+          HostName 1.mercury-web-backend.staging-earth.internal.mercury.com
+
+      Host mercury-prod-1
+          HostName 1.mercury-web-backend.production.internal.mercury.com
+
+      Host mercury-prod-2
+          HostName 2.mercury-web-backend.production.internal.mercury.com
+
+      Host mercury-staging-worker
+          HostName 1.mercury-web-backend-worker.staging.internal.mercury.com
+
+      Host mercury-prod-worker-1
+          HostName 1.mercury-web-backend-worker.production.internal.mercury.com
+
+      Host mercury-prod-worker-2
+          HostName 2.mercury-web-backend-worker.production.internal.mercury.com
+
+      Host mercury-prod-worker-3
+          HostName 3.mercury-web-backend-worker.production.internal.mercury.com
+
+      Host mercury-prod-worker-4
+          HostName 4.mercury-web-backend-worker.production.internal.mercury.com
+
+      Host mercury-prod-worker-5
+          HostName 5.mercury-web-backend-worker.production.internal.mercury.com
     '';
   };
 }
